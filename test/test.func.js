d33a86ba45409ad0f0ca4e706d9ea08981c40d47
Shannon Moeller <me@shannonmoeller.com>
2012-12-30 17:40:03 -0500

    Improve tests and test comments.

--- test/test.func.js
+++ test/test.func.js
@@ -13,25 +13,39 @@
 var Func = require('func');
 
 describe('Func()', function() {
-    it('should return an instance of Func', function() {
+    it('should always return an instance of Func', function() {
+        // Call normally
         assert(Func() instanceof Func);
+
+        // Call with new
+        assert(new Func() instanceof Func);
+
+        // Call and bind context
+        assert(Func.call(this) instanceof Func);
+        assert(Func.apply(null) instanceof Func);
     });
 
-    it('should be extendable', function() {
-        var sub = Func.extend({ a: 1 }, { b: 2 });
-        var ins = sub();
+    it('should be easily extendable', function() {
+        var prot = { a: 1 };
+        var stat = { b: 2 };
+
+        // Extend Func
+        var Sub = Func.extend(prot, stat);
+
+        // Create instance
+        var obj = Sub();
 
         // Check chain
-        assert.ok(ins instanceof Func);
-        assert.ok(ins instanceof sub);
+        assert.ok(obj instanceof Func);
+        assert.ok(obj instanceof Sub);
 
         // Check prototype
-        assert.strictEqual(sub.a, undefined);
-        assert.strictEqual(sub.prototype.a, 1);
-        assert.strictEqual(ins.a, 1);
+        assert.strictEqual(Sub.a, undefined);
+        assert.strictEqual(Sub.prototype.a, 1);
+        assert.strictEqual(obj.a, 1);
 
         // Check static
-        assert.strictEqual(sub.b, 2);
-        assert.strictEqual(ins.b, undefined);
+        assert.strictEqual(Sub.b, 2);
+        assert.strictEqual(obj.b, undefined);
     });
 });
