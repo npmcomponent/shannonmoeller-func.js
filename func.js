85bf12d3827562204a3a2a0ecc75e686b4a59a33
Shannon Moeller <me@shannonmoeller.com>
2012-12-30 17:43:50 -0500

    Added comments to func.js.

--- func.js
+++ func.js
@@ -29,36 +29,48 @@ var Func = function() {};
  * @return {Function}
  */
 Func.extend = function(prot, stat) {
+    // Whether to call this.init().
     var init = false;
 
+    // The essence of Object.create.
     var parent = new this();
 
+    // A function which always returns an instance.
     var fn = function F() {
         var self = this;
 
+        // Call with `new` if no one else has.
         if (!(self instanceof F)) {
+            // Don't execute the init logic.
             init = false;
 
+            // Just extend the prototype chain.
             self = new fn();
         }
 
         if (init) {
+            // Time to execute the init.
             self.init.apply(self, arguments);
         } else {
+            // Init next time around.
             init = true;
         }
 
+        // Return instance.
         return self;
     };
 
+    // Prototype (instance) properties.
     fn.prototype = copier(parent, prot, {
         constructor: fn
     });
 
+    // Static properties.
     copier(fn, stat, {
         extend: this.extend
     });
 
+    // Our shiney new constructor.
     return fn;
 };
 
